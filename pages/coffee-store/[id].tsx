4acb5194ed9d97b5d663b31be799589742428a3a
kayCodes11 <kayydraws@gmail.com>
2022-05-06 20:05:59 +0530

    getStaticProps added

--- pages/coffee-store/[id].tsx
+++ pages/coffee-store/[id].tsx
@@ -1,13 +1,38 @@
-import type {NextPage} from "next";
+import { useContext, useEffect, useState } from "react";
+import type {GetStaticProps, NextPage} from "next";
 import {useRouter} from "next/router";
+import coffeeStoresData from "../../data/coffee-stores.json";
 import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import styles from "../../styles/coffee-store.module.css";
+
+export const getStaticProps: GetStaticProps = (staticProps) => {
+    const params = staticProps.params;
+    console.log(params);
+    return {
+        props: {
+            coffeeStore: coffeeStoresData.find(coffeeStore => {
+                return coffeeStore.id.toString() === params?.id
+            })
+        }
+    }
+}
 
 const CoffeeStore: NextPage = () => {
-    const router = useRouter();
-    console.info(router);
+    const CoffeeStore = (props: {}) => {
+        const router = useRouter();
+        console.info(router);
+
+    }
+    const { address, name, neighbourhood } = props.CoffeeStore;
+
+    console.log(props);
     return (
         <div>
-            Coffee Store and id is {router.query.id}
+            <Head>
+                <title>{name}</title>
+            </Head>
             <Link href={"/"} scroll={false}>
                 <a>Back to home</a>
             </Link>
